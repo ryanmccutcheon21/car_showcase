ec4010bf1c86c78c183b68a66cd271dbc4bf7435
Ryan McCutcheon <ryanmccutcheon5@yahoo.com>
2023-08-25 18:20:20 -0500

    initial build

---
+++ components/Hero.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Image from '@/node_modules/next/image'
+
+const Hero = () => {
+    return (
+        <div className='hero'>Hero</div>
+    )
+}
+
+export default Hero
