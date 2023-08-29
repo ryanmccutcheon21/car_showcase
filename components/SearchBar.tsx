9848d7d08d251afcef9ea6f64298553075740162
Ryan McCutcheon <ryanmccutcheon5@yahoo.com>
2023-08-28 19:38:29 -0500

    create car catalog search logic

---
+++ components/SearchBar.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import React from 'react'
+import SearchManufacturer from './SearchManufacturer'
+import { useState } from 'react'
+
+const SearchBar = () => {
+    const [manufacturer, setManufacturer] = useState('')
+    const handleSearch = () => { }
+
+    return (
+        <form className='searchbar' onSubmit={handleSearch}>
+            <div className='searchbar__item'>
+                {/* Combo Box */}
+                <SearchManufacturer
+                    manufacturer={manufacturer}
+                    setManufacturer={setManufacturer}
+                />
+            </div>
+        </form>
+    )
+}
+
+export default SearchBar
