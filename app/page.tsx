d051c8e49a8507d018a1fc8f8cc5c6a5a6bb357a
Ryan McCutcheon <ryanmccutcheon5@yahoo.com>
2023-09-17 22:57:26 -0500

    create show more button

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Hero, CustomFilter, SearchBar, CarCard } from "@components"
+import { Hero, CustomFilter, SearchBar, CarCard, ShowMore } from "@components"
 import { fuels, yearsOfProduction } from "@constants"
 import { HomeProps } from "@types"
 import { fetchCars } from "@utils"
@@ -39,6 +39,10 @@ export default async function Home({ searchParams }: HomeProps) {
             <div className="home__cars-wrapper">
               {allCars?.map(car => <CarCard car={car} />)}
             </div>
+            <ShowMore
+              pageNumber={(searchParams.limit || 10) / 10}
+              isNext={(searchParams.limit || 10) > allCars.length}
+            />
           </section>
         ) : (
           <div className="home__error-container">
